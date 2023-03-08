264dfd7686b0baab403fdccc6fa4cd05d96d8f20
Jahir Fernando <91931363+Snx23@users.noreply.github.com>
2023-03-08 14:28:10 -0600

    Add files via upload

---
+++ Proyecto-final.py
@@ -0,0 +1,5 @@
+print("BIENVENIDO AL GENERADOR DE NOMBRE DE BANDAS")
+ciudad = input("Cual es el nombre de la ciudad donde naciste?: ")
+mascota = input("Cual es el nombre de tu mascota: ")
+
+print(f"El nombre de tu banda es {ciudad} {mascota}!!")
